6028f01611564ff3a759320d6046948ddcf355ed
Shayna Bergeron <shayna.celine@gmail.com>
2022-09-03 20:36:06 -0500

    Added an "Add Contact" field TODO: add styling

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='center'>
+                <h2>Contact Manager</h2>
+        </div>
+    );
+};
+
+export default Header;
