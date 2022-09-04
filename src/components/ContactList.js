c0964361ebeef55bc133c415f919885b017e0628
Shayna Bergeron <shayna.celine@gmail.com>
2022-09-04 14:30:32 -0500

    Created the contact card

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import List from '@mui/material/List';
-import Box from '@mui/material/Box';
 import './ContactList.css'
-import DeleteForeverOutlinedIcon from '@mui/icons-material/DeleteForeverOutlined';
+import ContactCard from './ContactCard';
 
 /** Displays the contacts as a list on the screen *
  * @todo: refactor any stylings                   *
@@ -10,17 +9,7 @@ import DeleteForeverOutlinedIcon from '@mui/icons-material/DeleteForeverOutlined
 
 const ContactList = (props) => {
    const renderContactList = props.contacts.map((contact) => {
-    return (
-        <Box  sx={{ p: 2, border: 0.5, borderColor: 'primary', borderRadius: '10px'}}>
-            <div>
-                <div className='contact-font-name'>{contact.name}</div>
-                <div className='contact-font-email'>{contact.email}</div>
-            </div>
-            <div className='div_relative'>
-            <DeleteForeverOutlinedIcon />
-            </div>
-        </Box>
-    );
+    return <ContactCard contact={contact}></ContactCard>;
    });
 
   
